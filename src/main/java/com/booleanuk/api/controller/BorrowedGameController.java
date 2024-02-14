7a2bc2a0849ed97d6577f9b9daef4962f02f8e78
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-14 14:48:39 +0100

    Create getAll for borrowedGame for users and games

--- src/main/java/com/booleanuk/api/controller/BorrowedGameController.java
+++ src/main/java/com/booleanuk/api/controller/BorrowedGameController.java
@@ -1,13 +1,32 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.dto.BorrowedGameDto;
+import com.booleanuk.api.model.BorrowedGame;
+import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.BorrowedGameRepository;
+import com.booleanuk.api.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("borrow")
 public class BorrowedGameController {
     @Autowired
     BorrowedGameRepository repository;
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<Response<List<BorrowedGameDto>>> getAllForUser(@PathVariable int userId) {
+        return ResponseEntity.ok(new Response<>(this.repository.findBorrowedGamesByUserId(userId)));
+    }
+
+    @GetMapping("/games/{gameId}")
+    public ResponseEntity<Response<List<BorrowedGameDto>>> getAllForGame(@PathVariable int gameId) {
+        return ResponseEntity.ok(new Response<>(this.repository.findBorrowedGamesByGameId(gameId)));
+    }
 }
--- src/main/java/com/booleanuk/api/repository/BorrowedGameRepository.java
+++ src/main/java/com/booleanuk/api/repository/BorrowedGameRepository.java
@@ -1,7 +1,23 @@
 package com.booleanuk.api.repository;
 
+import com.booleanuk.api.dto.BorrowedGameDto;
 import com.booleanuk.api.model.BorrowedGame;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BorrowedGameRepository extends JpaRepository<BorrowedGame, Integer> {
+    @Query("SELECT new com.booleanuk.api.dto.BorrowedGameDto(b.id, g.title, u.username, b.borrowDate, b.returnDate) " +
+            "FROM BorrowedGame b " +
+            "JOIN b.game g " +
+            "JOIN b.user u " +
+            "WHERE g.id = :gameId")
+    List<BorrowedGameDto> findBorrowedGamesByGameId(int gameId);
+    @Query("SELECT new com.booleanuk.api.dto.BorrowedGameDto(b.id, g.title, u.username, b.borrowDate, b.returnDate) " +
+            "FROM BorrowedGame b " +
+            "JOIN b.game g " +
+            "JOIN b.user u " +
+            "WHERE g.id = :userId")
+    List<BorrowedGameDto> findBorrowedGamesByUserId(int userId);
 }
