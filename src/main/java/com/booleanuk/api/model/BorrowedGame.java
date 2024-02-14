40a5cc4c6af206be10ddca9286f13af1e9bdd037
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-14 13:38:26 +0100

    set up BorrowedGame

--- src/main/java/com/booleanuk/api/model/BorrowedGame.java
+++ src/main/java/com/booleanuk/api/model/BorrowedGame.java
@@ -1,4 +1,37 @@
 package com.booleanuk.api.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "borrowed_games")
 public class BorrowedGame {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    private Game game;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private OffsetDateTime borrowDate;
+
+    @Column
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private OffsetDateTime returnDate;
+
 }
