43e6010e530f1d17389a23095ed175faa0104e57
Gustav Svennas <gustavsvennas@gmail.com>
2024-02-14 12:19:47 +0100

    Initial commit. Almost done with core

---
+++ src/main/java/com/booleanuk/library/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.booleanuk.library.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/booleanuk/library/model/User.java
@@ -0,0 +1,32 @@
+package com.booleanuk.library.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "borrowed_games")
+    private List<VideoGame> borrowedGames;
+
+    @Column(name = "prev_borrowed")
+    private List<VideoGame> prevBorrowed;
+}
