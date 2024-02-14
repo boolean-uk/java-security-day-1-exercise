01daa9c1de7082ffc85977e561b5b697ce217b10
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-14 14:00:56 +0100

    Create getAll endpoints for user and game

--- src/main/java/com/booleanuk/api/controller/GameController.java
+++ src/main/java/com/booleanuk/api/controller/GameController.java
@@ -1,13 +1,25 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.model.Game;
+import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.GameRepository;
+import com.booleanuk.api.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("games")
 public class GameController {
     @Autowired
     GameRepository repository;
+
+    @GetMapping
+    public ResponseEntity<Response<List<Game>>> getAll() {
+        return ResponseEntity.ok(new Response<>(this.repository.findAll()));
+    }
 }
--- src/main/java/com/booleanuk/api/controller/UserController.java
+++ src/main/java/com/booleanuk/api/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.UserRepository;
+import com.booleanuk.api.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
     @Autowired
     UserRepository repository;
+
+    @GetMapping
+    public ResponseEntity<Response<List<User>>> getAll() {
+        return ResponseEntity.ok(new Response<>(this.repository.findAll()));
+    }
 }
