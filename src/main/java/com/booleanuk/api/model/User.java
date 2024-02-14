1f728567ca56b11200054c58bfdcc514852a16c2
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-14 13:38:17 +0100

    set up User

--- src/main/java/com/booleanuk/api/model/User.java
+++ src/main/java/com/booleanuk/api/model/User.java
@@ -1,4 +1,40 @@
 package com.booleanuk.api.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private OffsetDateTime dob;
+
+    @OneToMany
+    private List<BorrowedGame> borrowedGames;
+
+
+    public User(String username, String email, OffsetDateTime dob) {
+        this.username = username;
+        this.email = email;
+        this.dob = dob;
+    }
 }
