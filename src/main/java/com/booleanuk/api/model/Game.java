27dc3208f6334e76bcb3ee773715f9c718c2a7ea
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-14 13:38:11 +0100

    set up Game

--- src/main/java/com/booleanuk/api/model/Game.java
+++ src/main/java/com/booleanuk/api/model/Game.java
@@ -1,4 +1,41 @@
 package com.booleanuk.api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "games")
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private int ageRating;
+
+    @Column
+    private String gameStudio;
+
+    @Column
+    private String genre;
+
+    @OneToMany
+    private List<BorrowedGame> borrowedGames;
+
+    public Game(String title, int ageRating, String gameStudio, String genre) {
+        this.title = title;
+        this.ageRating = ageRating;
+        this.gameStudio = gameStudio;
+        this.genre = genre;
+    }
 }
