212cf16a4560a60ad741ebce5011a6bc47ae5ace
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-14 14:59:17 +0100

    Create create endpoints for game and user

--- src/main/java/com/booleanuk/api/controller/GameController.java
+++ src/main/java/com/booleanuk/api/controller/GameController.java
@@ -1,15 +1,16 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.exception.ApiException;
 import com.booleanuk.api.model.Game;
 import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.GameRepository;
 import com.booleanuk.api.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,15 @@ public class GameController {
     @GetMapping
     public ResponseEntity<Response<List<Game>>> getAll() {
         return ResponseEntity.ok(new Response<>(this.repository.findAll()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Response<Game>> create(@RequestBody Game game) {
+        if (game.getGameStudio() == null || game.getTitle() == null || game.getGenre() == null || game.getAgeRating() <= 0) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "bad request");
+        }
+        Game createdGame = this.repository.save(game);
+        createdGame.setBorrowedGames(new ArrayList<>());
+        return new ResponseEntity<>(new Response<>(createdGame), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/booleanuk/api/controller/UserController.java
+++ src/main/java/com/booleanuk/api/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.exception.ApiException;
 import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.UserRepository;
 import com.booleanuk.api.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,5 +21,15 @@ public class UserController {
     @GetMapping
     public ResponseEntity<Response<List<User>>> getAll() {
         return ResponseEntity.ok(new Response<>(this.repository.findAll()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Response<User>> create(@RequestBody User user) {
+        if (user.getUsername() == null || user.getEmail() == null || user.getDob() == null) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "bad request");
+        }
+        User createdUser = this.repository.save(user);
+        createdUser.setBorrowedGames(new ArrayList<>());
+        return new ResponseEntity<>(new Response<>(createdUser), HttpStatus.CREATED);
     }
 }
